ef1f41348f62fcbab1fc9f3efb73cbf58cef3778
woorimj <128919405+woorimj@users.noreply.github.com>
2023-07-05 02:04:35 +0900

    Delete woorim directory

--- woorim/mungDiary/static/js/wrtie.js
+++
@@ -1,7 +0,0 @@
-const circleBtn = document.getElementById("write-form");
-
-function onClickBtn() {
-  window.location.href = "/pages/write.html?${circle}";
-}
-
-circleBtn.addEventListener("click", onClickBtn);
